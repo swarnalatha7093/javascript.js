369dc1d998b808e14fed48d77368048f4a69578a
swarnalatha7093 <145894664+swarnalatha7093@users.noreply.github.com>
2023-12-13 00:00:13 +0530

    regestration form js code12-12-23

---
+++ property_type.js
@@ -0,0 +1,35 @@
+/*
+#external example
+function showAlert(){
+    alert("Click action from JS file.");
+}
+#var example
+function showAlert(){
+    var name ="Swarna";
+    num = 10;
+    alert(name);
+    alert(num);
+}
+# let example
+function showAlert(){
+    let name ="Swarna";
+    num = 10;
+    alert(name);
+    alert(num);
+}
+#Hoisting example
+x = 10;
+var x=10;
+alert(x);
+*/
+//showAlertMessage();
+/*
+function showAlert(){
+    alert("Some Message")
+}
+
+function showAlert(){
+    const pi=3.14;
+    alert(pi);
+}
+*/
---
+++ reg_form.js
@@ -0,0 +1,11 @@
+function reg_form()  {
+    alert(document.getElementById("reg1").value);
+    alert(document.getElementById("reg2").value);
+    alert(document.getElementById("reg3").value);
+    alert(document.getElementById("reg4").value);
+    alert(document.getElementById("reg5").value);
+    alert(document.getElementById("reg6").value);
+    alert(document.getElementById("reg7").value);
+    alert(document.getElementById("reg8").value);
+    alert(document.getElementById("reg9").value);
+}
